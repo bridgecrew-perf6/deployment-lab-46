2a1704c51c7837dfd449e6255bad5cd6fa65eecc
camzakkour <camzakkour@gmail.com>
2022-02-23 13:29:46 -0800

    Added more content to page

--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ const app = express()
 app.get('/', function(req,res) {
     res.sendFile(path.join(__dirname, '../index.html'))
 })
+
 app.use(express.static(path.join(__dirname, '../')))
 
 const port = process.env.PORT || 3005
